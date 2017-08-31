037e7eac4cf1b6746e290185aeb2907775b0d2e6
xin7c <xin7c@qq.com>
2017-08-31 17:21:07 +0800

    折腾test results

--- tools/localException.py
+++ tools/localException.py
@@ -5,4 +5,4 @@ class ResponseIsNullException(Exception):
     def __init__(self, errorAppid=None):
         Exception.__init__(self)
         self.reason = u"[异常]:返回值为空Json\n[appid]%s" %errorAppid
-        print self.reason
+        # print self.reason
