90f61fbbb7085fca40e60c883aba0cb47a7fbfe3
xuchu <xuchu@reyun.com>
2017-09-08 15:10:47 +0800

    & select_reportName.py

--- tools/select_reportName.py
+++ tools/select_reportName.py
@@ -3,9 +3,21 @@
 
 import json
 import reportURI
+import requests
 
-uri = reportURI.uri
 
-print len(uri)
-for i, j in enumerate(uri):
-    print i + 1, j["reportname"], j["desc"]
+class ApiDocChecker():
+    def __init__(self):
+        self.apiUrl = "http://ec2-54-223-136-157.cn-north-1.compute.amazonaws.com.cn:8089/api/trackingio/allreportinfo"
+
+    def getApi(self):
+        r = requests.get(url=self.apiUrl)
+        uriList = r.text
+        print len(uriList)
+        for index, uri in enumerate(json.loads(uriList)):
+            print index + 1, ":", uri["reportname"], "^", uri["desc"]
+
+
+if __name__ == "__main__":
+    adc = ApiDocChecker()
+    adc.getApi()
